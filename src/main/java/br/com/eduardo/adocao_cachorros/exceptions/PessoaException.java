b83fc4b57512123b5d4a1bf7e87e7d282b108a7e
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2025-01-27 22:23:27 -0300

    Pessoa exception e service

---
+++ src/main/java/br/com/eduardo/adocao_cachorros/exceptions/PessoaException.java
@@ -0,0 +1,7 @@
+package br.com.eduardo.adocao_cachorros.exceptions;
+
+public class PessoaException extends RuntimeException {
+    public PessoaException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/eduardo/adocao_cachorros/services/PessoaService.java
@@ -0,0 +1,33 @@
+package br.com.eduardo.adocao_cachorros.services;
+
+import br.com.eduardo.adocao_cachorros.domain.Pessoa;
+import br.com.eduardo.adocao_cachorros.exceptions.PessoaException;
+import br.com.eduardo.adocao_cachorros.repository.PessoaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PessoaService {
+
+    private final PessoaRepository pessoaRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public void save(Pessoa pessoa){
+        pessoaRepository.save(pessoa);
+    }
+
+    public List<Pessoa> list(){
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas = pessoaRepository.findAll();
+        if(pessoas.isEmpty()) {
+            throw new PessoaException("Não há pessoas no banco de dados");
+        }
+        return pessoas;
+    }
+
+}
