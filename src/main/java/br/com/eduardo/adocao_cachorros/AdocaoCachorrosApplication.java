b21cc9ff5f79159e0c2516527e0e870e5b729942
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2025-01-27 22:07:33 -0300

    Aplicação

---
+++ src/main/java/br/com/eduardo/adocao_cachorros/AdocaoCachorrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.adocao_cachorros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoCachorrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocaoCachorrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduardo/adocao_cachorros/AdocaoCachorrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.adocao_cachorros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoCachorrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
